ff5d15e779172d98d43d02e3c6f77012dc6c81d4
HarryCandlish <hcnd90@gmail.com>
2019-10-01 13:23:43 +1300

    displaying event data

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import eventsListStyles from '../modules/eventsListStyles.module.scss'
+
 
 class EventsList extends Component {
     constructor() {
@@ -21,6 +23,9 @@ class EventsList extends Component {
             {this.state.events.map(event => (
                     <li key={event.id}>
                 <h1>{event.title}</h1>
+                <img className={eventsListStyles.eventsImage} alt="event" src={event.image}/>
+                <p>{event.description}</p>
+                <p>{event.date}</p>
                 </li>
             ))}
         </ul>
